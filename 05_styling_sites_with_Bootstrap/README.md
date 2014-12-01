5b04d9abacac7eb312f368f6d6bc8e7eb0cd737a
nick rothwell <nick@cassiel.com>
2014-12-01 12:41:18 +0000

    05 (duplicate check-in).

---
+++ 05_styling_sites_with_Bootstrap/README.md
@@ -0,0 +1,24 @@
+`-*- mode: markdown; mode: visual-line; mode: adaptive-wrap-prefix; -*-`
+
+# Building Sites with Bootstrap
+
+Bootstrap is a framework for building responsive, resizeable web sites. It's a combination of CSS (for styling) and Javascript (for interaction). It's a *front-end framework*: all the work is done in the browser (rather than the server).
+
+## Features
+
+- Layout template: a grid-based layout system.
+
+- Navigation: a standard set of tools for building navigation components.
+
+- HTML components (buttons, drop-down menus, tabs, ...).
+
+- Javascript components: dialog boxes, tooltips, ...
+
+- Responsiveness: pages are dynamically adapted for various screen sizes (from desktop to mobile).
+
+## Links
+
+- The definitive Bootstrap site is [here](http://getbootstrap.com/)
+- Sources are in the [GitHub repository](https://github.com/twbs/bootstrap)
+- [Wikipedia page](http://en.wikipedia.org/wiki/Bootstrap_(front-end_framework))
+- [Tutorial](http://www.w3schools.com/bootstrap/)
---
+++ 05_styling_sites_with_Bootstrap/presentation/presentation.clj
@@ -0,0 +1,249 @@
+(render :theme :sky
+        :title "Bootstrap"
+        :author "Nick Rothwell"
+
+        :slides [[:section
+                  (heading "The Bootstrap Framework")
+                  (subheading "Nick Rothwell")]
+
+                 [:section
+                  (heading "What is Bootstrap?")
+
+                  [:p "Bootstrap is a front-end tool set for building web applications"]
+                  [:p [:i "Front-end"] " means running in the browser, not the server.)"]
+
+                  (image-h 400 "link_user_1_fb37d.jpg")
+                  ]
+
+                 [:section
+                  (heading "Screen Shot")
+
+                  (image "stance.jpg")
+
+                  [:p "(Source: " (link "http://www.stance.com/") ")"]]
+
+                 [:section
+                  (heading "Modular Structure")
+
+                  [:ul
+                   [:li "Scaffolding (grid, ...)"]
+                   [:li "CSS (styling)"]
+                   [:li "Javascript (actions)"]
+                   [:li "Components (icons, ...)"]]]
+
+                 [:section
+                  (heading "Bootstrap encapsulates design standards")
+
+                  [:ul
+                   [:li.fragment "Grid layout"]
+                   [:li.fragment "Navigation bar"]
+                   [:li.fragment "Responsive (resizeable) design"]
+                   [:li.fragment "Reuseable controls (buttons, menus, ...)"]
+                   [:li.fragment "Active controls"]]]
+
+                 [:section
+                  [:section
+                   (heading "Installation")
+                   [:p "(1) Load from a CDN"]
+
+                   (include-code "bootstrap-cdn.html")]
+
+                  [:section
+                   (heading "Installation")
+                   [:p "(2) Download a precompiled copy"]
+
+                   (include-code "bootstrap-dirtree")]]
+
+                 [:section
+                  [:section
+                   (heading "Overview")]
+
+                  [:section
+                   (heading "Containers")
+
+                   (image "starter-template.jpg")
+                   (link "http://getbootstrap.com/examples/starter-template/")]
+
+                  [:section
+                   (heading "Navbars")
+
+                   (image "navbar.jpg")
+                   (link "http://getbootstrap.com/examples/navbar/")]
+
+                  [:section
+                   (heading "Grids")
+
+                   (image "grid.jpg")
+                   (link "http://getbootstrap.com/examples/grid/")]
+
+                  [:section
+                   (heading "The Bootstrap Theme")
+
+                   (image "theme.jpg")
+                   (link "http://getbootstrap.com/examples/theme/")]
+
+                  [:section
+                   (heading "The Jumbotron")
+
+                   (image "jumbotron.jpg")
+                   (link "http://getbootstrap.com/examples/jumbotron/")]
+
+                  [:section
+                   (heading "The Carousel")
+
+                   (image "carousel.jpg")
+                   (link "http://getbootstrap.com/examples/carousel/")]]
+
+                 [:section
+                  [:section
+                   (heading "The Starter Template")
+
+                   (image "starter-template.jpg")
+
+                   (link "http://getbootstrap.com/examples/starter-template/")]
+
+                  [:section
+                   (heading "Starter Template: Head")
+
+                   (include-code "starter-header.html")
+                   [:p "Note the relative paths (favicon, CSS)."]]
+
+                  [:section
+                   (heading "Starter Template: Navbar")
+
+                   (include-code "starter-navbar.html")]
+
+                  [:section
+                   (heading "Starter Template: Container")
+
+                   (include-code "starter-container.html")]
+
+                  [:section
+                   (heading "Starter Template: Javascript")
+
+                   (include-code "starter-js.html")
+                   [:p "Relative paths (again)."]]
+                  ]
+
+                 [:section
+                  [:section
+                   (heading "The Navbar")
+
+                   (link "http://getbootstrap.com/examples/navbar/")
+                   [:p "The Navbar is " [:i "responsive"] "."]
+                   [:p "(The document is modified with Javascript.)"]]]
+
+                 [:section
+                  [:section
+                   (heading "Containers")
+
+                   (link "http://getbootstrap.com/css/#overview-container")]
+
+                  [:section
+                   (heading "Containers (CSS)")
+
+                   [:p "Place the entire content within a " [:code "container"] " element"]
+                   (include-code "div-container.html")
+                   [:p "The container supports Bootstrap's grid system"]]]
+
+                 [:section
+                  [:section
+                   (heading "Exercise 1")
+                   (subheading "Make the template work!")
+
+                   [:p "Fix the starter template to correct the appearance"]
+                   (image-h 360 "starter-broken.jpg")
+                   [:p "(Firefox (and Chrome) are your friends - use the console.)"]
+                   [:p "See the " [:a {:href "https://github.com/codezoners-2/HTML/tree/master/05_styling_sites_with_Bootstrap/assignments/starter-template"} "README"] "."]]]
+
+                 [:section
+                  [:section
+                   (heading "The Grid System")
+
+                   (link "http://getbootstrap.com/css/#grid")]
+
+                  [:section
+                   (heading "Grid Rows and Columns")
+
+                   (include-code "col-md.html")]
+
+                  [:section
+                   (image-h 300 "simple-grid.jpg")
+
+                   [:p "All rows are 12 columns wide"]
+                   [:p "Rows will stack on smaller devices"]]
+
+                  [:section
+                   (subheading "Control Stacking on Smaller Devices")
+
+                   [:ul
+                    [:li (tt ".col-lg-*") " (large devices - desktops)"]
+                    [:li (tt ".col-md-*") " (medium devices - desktops)"]
+                    [:li (tt ".col-sm-*") " (small devices - tablets)"]
+                    [:li (tt ".col-xs-*") " (extra-small devices - phones)"]]]
+
+                  [:section
+                   (heading "Offsetting Columns")
+
+                   (include-code "grid-offsets.html")]
+
+                  [:section
+                   (image-h 300 "grid-offsets.png")
+
+                   [:p (tt ".col-md-offset-*") " (or " (tt "-lg-") ", ...) moves"
+                    " columns to the right"]]
+
+                  [:section
+                   (subheading "Fluid Container")
+
+                   [:p "Use " (tt ".container-fluid") " for full-width containers"]
+
+                   (include-code "container-fluid.html")]]
+
+                 [:section
+                  [:section
+                   (heading "Exercise 2")
+                   (subheading "Fun with the Grid")
+
+                   [:p "Can you create these patterns using Bootstrap's Grid?"]]
+
+                  [:section
+                   (image "grid-fun-1.png")]
+
+                  [:section
+                   (image "grid-fun-2.png")]
+
+                  [:section
+                   (subheading "Hints")
+
+                   [:ul
+                    [:li "The colour gradients are done with "
+                     (tt "background: linear-gradient(#......, #......)")]
+                    [:li "The rounded corners are " (tt "border-radius: 0.5em")]
+                    [:li "There's a margin, and a bit of padding, above and/or below the cells"]]]
+
+                  ]
+
+                 [:section
+                  [:section
+                   (heading "Exercise 3 (Group Exercise!)")
+                   (subheading "A Facebook Clone")
+
+                   (image "codezonersbook.png")]
+
+                  [:section
+                   (subheading "Bring Everything Together")
+
+                   [:ul
+                    [:li "Use the Starter Template"]
+                    [:li "Build a navigation bar with drop-down menu"]
+                    [:li "Lay out the content in three columns"]]]
+
+                  [:section
+                   (subheading "Group Exercise!")
+
+                   [:ul
+                    [:li "Write a biog page each"]
+                    [:li "Upload them to our tame server"]
+                    [:li "Swap URLs for your pages"]
+                    [:li "Create a drop-down menu containing a link to each page"]]]]])
